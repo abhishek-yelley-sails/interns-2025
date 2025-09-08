4d9cf64c81f388fac1045eacc9e40eb57a277acb
raha-mohammad <rahamatunnisa.mohammad@sailssoftware.com>
2025-09-08 17:46:04 +0530

    first commit

--- week-1/index.js
+++ week-1/index.js
@@ -1,5 +1,9 @@
 function solution1(name, score) {
+    const outcome = score >= 15 ? "passed" : "failed";
   // your solution here
+  return `${name} ${outcome} with a score of ${score}`;
+
+
 }
 
 document
@@ -7,7 +11,10 @@ document
   .addEventListener("click", () => runTest1(solution1));
 
 function solution2(name, score, template) {
+  const outcome = score >= 15 ? "passed" : "failed";
   // your solution here
+  return template.replace("{{name}}", name).replace(/{{score}}/g, score).replace("{{outcome}}", outcome);
+
 }
 
 document
